3c847f43ef91077cfdfc6e8325c1df44dc399c07
benji <benji.b.ramirez@gmail.com>
2017-05-30 23:56:28 -0700

    refactored player, ball, blocks, using inheritance on rectangle

--- breakout/main.cpp
+++ breakout/main.cpp
@@ -9,51 +9,50 @@
 #include <ctime>
 #include <unistd.h>
 
-#define WIN_WIDTH 400
+#define WIN_WIDTH 600
 #define WIN_HEIGHT 600
-
 #define NUM_LAYERS 3
 #define NUM_COLS 7
+#define BLOCK_OFFSET 50
+#define BLOCK_HEIGHT 15
 
-
-struct Player
+struct Rectangle
 {
-
-    sf::Vector2f velocity;
     sf::RectangleShape body;
-    int leftBound;
-    int rightBound;
-    
-    Player(sf::Vector2f size, float x_position, float y_position, int leftBound, int rightBound)
-    {    
-        this->body.setSize(size);
-        this->body.setPosition(x_position, y_position);
-        this->leftBound = leftBound;
-        this->rightBound = rightBound;
-    };
+    float getX(){return body.getPosition().x;}
+    float getY(){return body.getPosition().y;}
+    float getLeft(){return getX();}
+    float getRight(){return getX() + body.getLocalBounds().width;}
+    float getTop(){return getY();}
+    float getBottom(){return getY() + body.getLocalBounds().height;}
+    float getWidth(){return body.getLocalBounds().width;}
+    float getHeight(){return body.getLocalBounds().height;}
+};
+
+struct Player: public Rectangle
+{
+    sf::Vector2f velocity;
+
+    Player(sf::Vector2f size, float x, float y)
+    {
+        body.setSize(size);
+        body.setPosition(x, y);
+        body.setFillColor(sf::Color::Blue);
+    }
 
     void update()
     {
-        this->body.setPosition(this->body.getPosition() + this->velocity);
-        this->checkBoundaries();
-        this->stop();
-        // std::cout << this->body.getPosition().x << std::endl;
+        body.move(velocity);
+        checkBoundaries();
+        stop();
     }
+    
+    void stop(){velocity.x = 0;}
 
     void checkBoundaries()
-    { // keep player inside window
-        
-        if(this->body.getPosition().x > (this->rightBound - this->body.getLocalBounds().width))
-        {
-            this->body.setPosition(this->rightBound - this->body.getLocalBounds().width, this->body.getPosition().y);
-        
-        }
-        else if(this->body.getPosition().x < this->leftBound)
-        {
-
-            this->body.setPosition(this->leftBound, this->body.getPosition().y);
-        }
-
+    {
+        if(getLeft() < 0){ body.setPosition(0, getY()); }
+        if(getRight() > WIN_WIDTH){ body.setPosition(WIN_WIDTH - getWidth(), getY()); }
     }
 
     void move(bool left, bool right)
@@ -61,63 +60,87 @@ struct Player
         if(left) this->velocity.x = -5;
         if(right) this->velocity.x = 5;
     }
-
-    void stop(){this->velocity.x = 0;}
-
 };
 
+struct Ball: public Rectangle // a Rectangular Ball...
+{
+    sf::Vector2f velocity;
 
-void initBlocks( sf::RectangleShape blocks[][NUM_COLS]){
-    // the 2d vector of blocks
-    float blockWidth = WIN_WIDTH/NUM_COLS - 1;
-    float blockHeight = (WIN_HEIGHT/NUM_LAYERS)/6 -1;
-    float vertOffset = 40;
-    sf::Vector2f blockSize( blockWidth, blockHeight );
+    Ball(sf::Vector2f size, float x, float y, float vX, float vY)
+    {
+        body.setSize(size);
+        body.setPosition(x, y);
+        body.setFillColor(sf::Color::White);
+        velocity.x = vX;
+        velocity.y = vY;
+    }
+
+    void update()
+    {
+        checkBoundaries();
+        body.move(velocity);
+    }
+
+    void checkBoundaries()
+    {
+        // left and right wall collisions
+        if(getLeft() < 0){body.setPosition(0, getY()); velocity.x *= -1; }
+        if(getRight() > WIN_WIDTH){body.setPosition(WIN_WIDTH - getWidth(), getY()); velocity.x *= -1;}
+        // top bottom wall collisions
+        if(getBottom() > WIN_HEIGHT){body.setPosition(getX(), WIN_HEIGHT - getHeight()); velocity.y *= -1;}
+        if(getTop() < 0){body.setPosition(getX(), 0 ); velocity.y *= -1;}
+    }
+};
+
+struct Block: public Rectangle
+{
+    bool alive = true;
+
+    Block(sf::Vector2f size, float x, float y)
+    {
+        body.setSize(size);
+        body.setPosition(x, y);
+        body.setFillColor(sf::Color::Red);
+    }
+};
+
+void initBlocks( std::vector<Block>& blocks){
+
+    sf::Vector2f blockSize(WIN_WIDTH/NUM_COLS, BLOCK_HEIGHT);
     for(int i = 0; i < NUM_LAYERS; i++)
     {
         for(int j = 0; j < NUM_COLS; j++)
         {
-            float x_position = ((float)j/NUM_COLS) * (float)WIN_WIDTH;
-            std::cout << j << std::endl;
-                        std::cout << x_position << std::endl;
-
-            float y_position = vertOffset + ((float)i * blockHeight) + i;
-            blocks[i][j].setSize(blockSize);
-            blocks[i][j].setPosition(x_position, y_position);
+            float x = ((float)j/NUM_COLS) * (float)WIN_WIDTH + j;
+            float y = BLOCK_OFFSET + ((float)i * BLOCK_HEIGHT) + i;
+            Block block(blockSize, x, y);
+            blocks.push_back(block);
         }
     }
-
 }
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT), "Breakout Clone");
 
-    float characterWidth = 15.f;
     // the player rectangle
-    sf::Vector2f playerSize(100.f, 15.f);
-    Player player(playerSize, WIN_WIDTH/2, WIN_HEIGHT - playerSize.x, 0, WIN_WIDTH);
+    sf::Vector2f playerSize(100.f, BLOCK_HEIGHT);
+    Player player(playerSize, WIN_WIDTH/2, WIN_HEIGHT - playerSize.x);
 
-    // the ball rectangle
-    sf::Vector2f ballSize(characterWidth, characterWidth);
-    sf::RectangleShape ball(ballSize);
-    ball.setPosition(WIN_WIDTH/2, WIN_HEIGHT/2 );
-    //allocate memory then set block size and positions
-    sf::RectangleShape blocks[NUM_LAYERS][NUM_COLS]; 
+    // testing out the amazing rectangular ball
+    sf::Vector2f ballSize(BLOCK_HEIGHT, BLOCK_HEIGHT);
+    Ball ball(ballSize, WIN_WIDTH/2, WIN_HEIGHT/2 , -4.f, 4.f);
+
+    std::vector<Block> blocks;
     initBlocks(blocks); 
 
     // set up objects for keeping constant FPS
-    sf::Clock gameClock;
-    long SKIP_TICKS = 10000;
-
-
+    window.setFramerateLimit(60);
 
     std::cout << "Game is starting!\n";
 
     while (window.isOpen())
     {
-        long startTime = gameClock.getElapsedTime().asMilliseconds();
-
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -139,21 +162,22 @@ int main()
         }
 
         player.update();
+        ball.update();
         // erase old render
         window.clear();
         // draw the updates
         window.draw(player.body);
-        window.draw(ball);
+        window.draw(ball.body);
+
         for(int i = 0; i < NUM_LAYERS; i++){
             for (int j = 0; j < NUM_COLS; j++){
-                window.draw(blocks[i][j]);
+                if(blocks[i*NUM_COLS + j].alive)
+                    window.draw(blocks[i*NUM_COLS + j].body);
             }
         }
-        // display the updates
+
         window.display();
         
-        long endTime = gameClock.getElapsedTime().asMilliseconds();
-        usleep(SKIP_TICKS - (endTime - startTime));
     }
 
     return 0;
